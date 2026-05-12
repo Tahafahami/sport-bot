44f81e17574a4b57601b7f6e68a7435beebbd6cc
Taha Fahami <tahafahami@Tahas-Mac-mini.local>
2026-05-12 12:33:13 +0330

    clean fixed bot

--- bot.py
+++ bot.py
@@ -1,21 +1,13 @@
 from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    ContextTypes,
-    filters,
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = "توکن خودت"
+TOKEN = "8297614142:AAFtxrsxgBoZuW6gUjdHAQhR9_Y8I-XUnsY"
 
-# استارت
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     keyboard = [
         ["❤️ سوالات قلب", "🌬 سوالات تنفس"],
-        ["📚 جزوه ها", "☎️ پشتیبانی"],
-        ["🧠 سوالات مغز", "🦴 سوالات استخوان"]
+        ["📚 جزوه ها", "☎️ پشتیبانی"]
     ]
 
     reply_markup = ReplyKeyboardMarkup(
@@ -28,42 +20,30 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=reply_markup
     )
 
-# جواب دکمه ها
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     text = update.message.text
 
     if text == "❤️ سوالات قلب":
         await update.message.reply_text(
-            "📖 سوالات قلب:\n\n1- قلب چند حفره دارد؟\n2- وظیفه قلب چیست؟"
+            "1- قلب چند حفره دارد؟\n2- وظیفه قلب چیست؟"
         )
 
     elif text == "🌬 سوالات تنفس":
         await update.message.reply_text(
-            "📖 سوالات تنفس:\n\n1- ریه چه کاری انجام میدهد؟"
+            "1- ریه چه کاری انجام میدهد؟"
         )
 
     elif text == "📚 جزوه ها":
         await update.message.reply_text(
-            "📎 جزوه ها به زودی اضافه میشوند"
+            "جزوه ها به زودی اضافه میشوند"
         )
 
     elif text == "☎️ پشتیبانی":
         await update.message.reply_text(
-            "📞 آیدی پشتیبانی:\n@yourid"
+            "@yourid"
         )
 
-    elif text == "🧠 سوالات مغز":
-        await update.message.reply_text(
-            "🧠 سوالات مغز اینجاست"
-        )
-
-    elif text == "🦴 سوالات استخوان":
-        await update.message.reply_text(
-            "🦴 سوالات استخوان اینجاست"
-        )
-
-# اجرای بات
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
@@ -75,3 +55,4 @@ app.add_handler(
 print("Bot running...")
 
 app.run_polling()
+
