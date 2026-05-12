70b4aef7057278204b107ab79164a138deaf66a2
Taha Fahami <tahafahami@Tahas-Mac-mini.local>
2026-05-12 12:49:21 +0330

    downgrade telegram bot

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = "8297614142:AAFtxrsxgBoZuW6gUjdHAQhR9_Y8I-XUnsY"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+# استارت
+def start(update, context):
 
     keyboard = [
         ["❤️ سوالات قلب", "🌬 سوالات تنفس"],
@@ -15,44 +16,45 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         resize_keyboard=True
     )
 
-    await update.message.reply_text(
+    update.message.reply_text(
         "سلام 👋\nچطور میتونم کمکتون کنم؟",
         reply_markup=reply_markup
     )
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+# دکمه ها
+def handle_message(update, context):
 
     text = update.message.text
 
     if text == "❤️ سوالات قلب":
-        await update.message.reply_text(
+        update.message.reply_text(
             "1- قلب چند حفره دارد؟\n2- وظیفه قلب چیست؟"
         )
 
     elif text == "🌬 سوالات تنفس":
-        await update.message.reply_text(
+        update.message.reply_text(
             "1- ریه چه کاری انجام میدهد؟"
         )
 
     elif text == "📚 جزوه ها":
-        await update.message.reply_text(
+        update.message.reply_text(
             "جزوه ها به زودی اضافه میشوند"
         )
 
     elif text == "☎️ پشتیبانی":
-        await update.message.reply_text(
+        update.message.reply_text(
             "@yourid"
         )
 
-app = ApplicationBuilder().token(TOKEN).build()
+# اجرای بات
+updater = Updater(TOKEN, use_context=True)
 
-app.add_handler(CommandHandler("start", start))
+dp = updater.dispatcher
 
-app.add_handler(
-    MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
-)
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(MessageHandler(Filters.text, handle_message))
 
 print("Bot running...")
 
-app.run_polling()
-
+updater.start_polling()
+updater.idle()
